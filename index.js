9d69ecddbb5f603c30f597cb02befe3a057b5400
Carlwilliam <jdcarlwilliam@gmail.com>
2023-09-21 10:49:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 function distanceFromHqInBlocks(someValue) {
-    return Math.abs(someValue - 42);
+  return Math.abs(someValue - 42);
 }
 
 function distanceFromHqInFeet(someValue) {
-   const feet = distanceFromHqInBlocks(someValue);
-return feet * 264;
-  }
-  function distanceTravelledInFeet(start, destination) {
-    return Math.abs(destination-start) * 264
-    
-  }
-  function calculatesFarePrice(start, destination) {
-    const feet = distanceFromHqInBlocks(start, destination);
+  const blocks = distanceFromHqInBlocks(someValue);
+  return blocks * 264;
+}
 
-    if(feet <= 400){
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const feet = distanceTravelledInFeet(start, destination);
+
+  if (feet < 400) {
       return 0;
-    }
-    else if (feet > 400 && feet <= 2000){
+  } else if (feet >= 400 && feet <= 2000) {
       return (feet - 400) * 0.02;
-    }
-    else if (feet > 2000 && feet <= 2500){
+  } else if (feet > 2000 && feet <= 2500) {
       return 25;
-
-     }else {
+  } else {
       return 'cannot travel that far';
-     }
   }
-
-    
-
-    
-  
+}
